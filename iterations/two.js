420661628ab0a4349520265658f9318839c8afc3
KshitijChavan-Stack <chavankshitij0@gmail.com>
2025-10-01 16:18:47 +0530

    Loops and keywords used

---
+++ iterations/two.js
@@ -0,0 +1,20 @@
+// let index = 0;
+// while (index <= 10) {
+//   console.log(`value of index is ${index}`);
+//   index++; // increment / termination
+// }
+
+// let myArr = ["flash", "superman", "spiderman"];
+
+// let arr = 0;
+// while (arr < myArr.length) {
+//   console.log(`Hero name is ${myArr[arr]}`);
+//   arr += 1;
+// }
+
+let score = 11;
+// this will atleast work ones
+do {
+  console.log(`score is ${score}`);
+  score += 1;
+} while (score <= 10);
